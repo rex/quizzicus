2d4b96fbc3d50e7b4268f5ec769540f17f907e96
Pierce Moore <piercemoore@gmail.com>
2012-04-14 02:08:30 -0500

    Created a script to build quizzes from scratch, complete with answers and settings. Man, this is a good day.

--- application/controllers/autopop.php
+++ application/controllers/autopop.php
@@ -3,6 +3,122 @@
 class AutoPop extends CI_Controller {
 	
 	public function index() {
+		print "Select a function: <br /><br />";
+		print '<a href="' . site_url("autopop/users") . '">Create a lot of users</a><br /><br />';
+		print '<a href="' . site_url("autopop/quizzes") . '">Create a lot of quizzes</a><br /><br />';
+	}
+
+	public function quizzes() {
+
+		$num = new stdClass;
+		$num->quizzes = 100;
+		$num->questions = 20;
+		$num->answers = 5;
+
+		$quiz = new stdClass;
+		$quiz->subjects = array(
+			'Language Arts','Mathematics','Science','Health','Handwriting','Physical Education','Art','Music','Instrumental Music','Movement or Eurythmy','Handwork or handcrafts','Life Lab or gardening','Dramatics',
+			'Dance','Spanish or other foreign language','Leadership','Special Education Day Class','Resource Program','Speech','Adaptive P.E.','Occupational Therapy','Middle School Subjects','Reading','Language arts',
+			'Speech and Debate','English','Basic Math','Pre-algebra','Consumer Math','Algebra','Geometry','Honors Math in Algebra or Geometry','Life Science','Earth Science','Physical Science','Health','Social Studies',
+			'Geography','Ancient Civilizations','Medieval and Renaissance','U.S. History and Government','French / Spanish / Latin','Computer Science or Lab','Art','Home Economics','Woodshop','Metal Shop','Business Technology',
+			'Instrumental Music','Band','Choir','Drama','Physical Education','Sports','Special Education Day Class','Resource Program','Speech Therapy','Occupational Therapy','English I','English II','English III','English IV',
+			'Remedial English','ESL – English as second language','World Literature','Ancient Literature','Medieval Literature','Renaissance Literature','Modern Literature','British Literature','American Literature','Classical literature',
+			'Shakespeare','Heroes, Myth and Legend','Film as Literature','Composition','Creative Writing','Poetry','Grammar','Vocabulary','Debate','Speech','Journalism','Publishing Skills','Photojournalism','Yearbook','Study Skills','Research Skills',
+			'FINE ARTS','Art I','Art II','Art III','Art IV','Art Appreciation','Art History','Drawing','Painting','Sculpture','Ceramics','Instrumental Music','Music Appreciation','Music History','Music Theory','Music Fundamentals','Band',
+			'Orchestra','Choir','Voice','Classical Music Studies','Performing Arts','Theatre Arts'
+		);
+		$quiz->types = array(
+			"Exam" , "Final" , "Midterm" , "Study Guide" , "Pop Quiz" , "Quiz" , "Entrance Exam" , "Knowledge Assessment"
+		);
+		$quiz->questions = array(
+			"Where is the United States?","Who is the president?","Why is the sky blue?","How many woodchucks are there?","Are there aliens out there?", "How long is your name?","Who was our first president?","What is my name?",
+			"Who is your daddy?","What does he do?","What does a keyboard look like?","How bright is the sun?","Why are there muskrats everywhere?","Is this a bizarre question?","This statement is false.","GlaDOS is amazing","The sun is yellow.",
+			"There are 200 letter in the English language.","I love snack cakes.","I wonder how many planets there are?","This is a question.","This is not a question.","I wonder if this is a question?","How many people are there?","Do you like coca-cola?",
+			"How do you like your burgers cooked?", "Do you own a dog?","What is the subject of this class?","Blah blah blah HAHAHA","WOOOOOOO Going crazy","I like lightbulbs.","I need some advil.","What is Advil made of?","What's the VP's name?",
+			"Do you love me?","Are you playing your love games with me?","Do you know what love games are?"
+		);
+		$surnames = array("Mr." , "Mrs.");
+		$fnames = array(
+			"Sally" , "Bobby" , "Mark" , "Joe" , "Rebecca" , "Elizabeth" , "Sarah" , "Travis" , "Billy" , "Nathan" , "Yu" , "Josh" , "Ian" , "Jacob" , 
+			"Michael" , "Scott" , "Matthew" , "Ethan" , "Andrew" , "Daniel" , "Anthony" , "Christopher" , "Alexander" , "Ryan" , "Tyler" , "Dylan" , "Benjamin"
+		);
+		$lnames = array(
+			"Moore" , "Fisher" , "Hamilton" , "Darvish" , "Kinsler" , "Garcia" , "Jones" , "Mercer" , "Alameel" , "Davis" , "Johnson" , "Sills" , "Williams" , "Brown" , "Miller" , 
+			"Anderson" , "Jackson" , "Martin" , "Harris" , "Thompson" , "Wilson" , "Taylor" , "Robinson" , "Martinez" , "Clark" , "Lewis" , "Lee"
+		);
+		$streets = array(
+			"Oak" , "Spring Valley" , "Canton" , "Oak Lawn" , "Main" , "Belt Line" , "Clark" , "Cedar" , "Meadow Creek" , "Creek View" , "Valley View" , "Commerce" , "Gaston" , 
+			"Awesome" , "Captain Amazing" , "Whatever" , "Baseball" , "Ballpark" , "Prairie" , "Spring Creek" , "Snob Hill" , "Swiss" , "Tree Grass" , "Lollipop" , "Happiness" , "Bay Point"
+		);
+
+
+		$finished = false;
+
+		$quizzes = array();
+		while( !$finished ) {
+
+			$questions = "";
+			$quizCount = count($quizzes);
+			$rand = rand(0,10000);
+			$teacher = array_rand(array_flip( $surnames )) . " " . array_rand(array_flip( $lnames ));
+			$subject = array_rand(array_flip( $quiz->subjects ));
+			$quiztype = array_rand(array_flip( $quiz->types ));
+			$date = date("Y-m-d H:i:s");
+			$active = ( ( $quizCount % 10 ) == 0 ) ? "0" : "1";
+			$public = ( ( $quizCount % 5 ) == 0) ? "0" : "1";
+			$timed = ( ( $quizCount % 3 ) == 0) ? "0" : "1";
+			$timer = ( ( $quizCount % 7 ) == 0) ? "0" : 60 * rand(60,6000);
+			$allowBreaks = ( ( $quizCount % 4 ) == 0) ? "0" : "1";
+			$num_breaks = ( $allowBreaks == "0" ) ? "0" : rand(1,100);
+			$randomize = ( ( $quizCount % 6 ) == 0) ? "0" : "1";
+			$nsfw = ( ( $quizCount % 8 ) == 0) ? "1" : "0";
+
+			$testname = "$teacher's $subject $quiztype";
+			for( $i = 0; $i < $num->questions; $i++ ) {
+
+				if( $i % 5 == 0 ) {
+					$type = 'bool';
+					$answers = array();
+				} else {
+					$type = 'mult';
+					$answers = array_rand(array_flip( array_merge($fnames,$streets,$lnames,$quiz->subjects) ), $num->answers );
+				}
+				$questions[] = array(
+					'name'	=>	array_rand(array_flip( $quiz->questions )),
+					'type'	=>	$type,
+					'desc'	=>	"This is a question description. I really hope you enjoy it, because I'm not making a million custom descriptions.",
+					'answers' => $answers
+				);
+			}
+			$data = array(
+				'name'	=>	$testname,
+				'questions' => $questions,
+				'description' => "The test description goes here. For this one, we would make a reference to the title, $testname.",
+				'num_questions' => $num->questions,
+				'date_created'	=>	$date,
+				'created_by_user' => $rand,
+				'last_modified_by_user' => $rand,
+				'active' => $active,
+				'public' => $public,
+				'timed'	=> $timed,
+				'timer'	=> $timer,
+				'allow_breaks'	=> $allowBreaks,
+				'num_breaks_allowed' => $num_breaks,
+				'randomize'	=> $randomize,
+				'nsfw'	=> $nsfw
+			);
+			$quizzes[] = $data;
+
+			if( count($quizzes) >= $num->quizzes ) {
+				$finished = true;
+			}
+
+		}
+
+		print_r( $quizzes );
+	}
+
+	public function users() {
 
 		// Grab Country Codes
 		$q = $this->db->select('iso')->from('taxonomy_countries')->get();
