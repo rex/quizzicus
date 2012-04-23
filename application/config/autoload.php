efb81eb182684a71aef3e92a73ebd6b2b633a660
Pierce Moore <Pierce@PierceMoore.com>
2012-04-23 12:41:53 -0500

    Working on MongoDB-based sessions.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('session','pageload','mongo_db');
+$autoload['libraries'] = array('session','pageload','mdb');
 
 
 /*
@@ -80,7 +80,7 @@ $autoload['helper'] = array('url');
 |
 */
 
-$autoload['config'] = array();
+$autoload['config'] = array('mdb');
 
 
 /*
---
+++ application/config/mdb.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['mdb_host'] = '127.0.0.1';
+$config['mdb_port'] = 27017;
+$config['mdb_db'] = 'quiz';
+$config['mdb_user'] = '';
+$config['mdb_pass'] = '';
+$config['mdb_default_collection'] = "quiz";
+$config['mdb_session_enable'] = true;
